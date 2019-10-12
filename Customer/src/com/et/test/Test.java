e265ab0769b2bd772a1bfa18b67566fb463578b8
Brain <boqipeng@etoak.cn>
2019-10-12 14:26:41 +0800

    测试方法添加

---
+++ Customer/src/com/et/test/Test.java
@@ -0,0 +1,12 @@
+package com.et.test;
+
+/**
+ * @AUTHOR Brain
+ * @DATE 2019/10/12 - 14:25
+ **/
+
+public class Test {
+    public void test(){
+        
+    }
+}
--- Customer/src/com/et/test/TestAuthor.java
+++ Customer/src/com/et/test/TestAuthor.java
@@ -16,6 +16,14 @@ public class TestAuthor {
      * 用户名
      */
     private String name = "小百";
+
+
+    public static void main(String[] args) {
+        int id = 2;
+        String name = "小新";
+        System.out.println(id + name);
+
+    }
     
     public void testUpdate(){
         System.out.println(id + name);
